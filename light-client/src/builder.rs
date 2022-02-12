026dd1b05f1ec829d91767b60a290d8d3b0845b1
charlie <charlie@findora.org>
2022-02-11 19:44:15 -0600

    points to forked anomaly v0.2.0

--- light-client/src/builder.rs
+++
@@ -1,9 +0,0 @@
-//! DSL for building light clients and supervisor
-
-mod light_client;
-pub use light_client::LightClientBuilder;
-
-mod supervisor;
-pub use supervisor::SupervisorBuilder;
-
-pub mod error;
--- light-client/src/components.rs
+++
@@ -1,6 +0,0 @@
-//! Components used by the Light Client.
-
-pub mod clock;
-pub mod io;
-pub mod scheduler;
-pub mod verifier;
